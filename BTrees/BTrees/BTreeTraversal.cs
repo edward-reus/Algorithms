f846f39d29bf5f8079fa92b370295910456ee3f9
Edward Reus <Edward Reus>
2015-10-28 14:52:59 -0700

    Initial csproj for btree traversal.

---
+++ BTrees/BTrees/BTreeTraversal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTrees
+{
+    public class BTreeTraversal: iBTreeTraversal
+    {
+        public void InOrder(Node n)
+        {
+            return;
+        }
+    }
+}
