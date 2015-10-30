93262dbf3585f46085c34dde7b09f709e91dba5a
Edward Reus <edwardr@ntdev.microsoft.com>
2015-10-30 10:28:24 -0700

    Add new class for balanced trees. Add tree depth method.

---
+++ BTrees/BTrees/BalancedTrees.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTrees
+{
+    public class BalancedTrees
+    {
+        // The depth of the tree is the longest chain of nodes in the b-tree.
+        // This recursive method adds one for each node "level" to find the depth.
+        public int TreeDepth(Node tree)
+        {
+
+            if (tree == null)
+            {
+                return 0;
+            }
+
+            int dLeft = TreeDepth(tree.left);
+            int dRight = TreeDepth(tree.right);
+
+            return Math.Max(dLeft, dRight) + 1;  // Plus one for this node...
+        }
+    }
+}
